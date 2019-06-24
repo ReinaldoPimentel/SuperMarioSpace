32cbc92d4a81744547b8024cf96b42132b604256
Miguel Vicente <vicente_miguel@hotmail.com>
2019-06-24 00:06:34 +0000

    score

--- src/org/academiadecodigo/SuperMarioPlanes/Main.java
+++ src/org/academiadecodigo/SuperMarioPlanes/Main.java
@@ -22,9 +22,11 @@ public class Main {
 
         SimpleGfxAirArena arena = new SimpleGfxAirArena();
         arena.init();
-        PlayerPlane b = new PlayerPlane(arena, arena.makeGridPosition(25, 65, "resources/plani.png"));
+        PlayerPlane b = new PlayerPlane(arena, arena.makeGridPosition(25, 65, "resources/player1.png"));
         AudioX audio = new AudioX();
         int counter = 0;
+        double levelDiff = 0.05;
+        int difficulty = (int) (Math.random() * 100);
 
 
 
@@ -40,51 +42,54 @@ public class Main {
 
 
 
+            while (!b.isDead()) {
+                Text score = new Text(110, 50, "SCORE: " + counter);
+                score.grow(100, 25);
+                score.draw();
 
-        while (!b.isDead()) {
+                try {
+                    Thread.sleep(40);
+                    //threadCounter += 40;
+                    arena.move();
 
-            try {
-                Thread.sleep(40);
-                arena.move();
+                    if (Math.random() < 0.9) {
 
-                if (Math.random() < 0.05) {
+                        EnemyPlane enemy = PlaneFactory.getNewPlane(arena);
 
-                    EnemyPlane enemy = PlaneFactory.getNewPlane(arena);
+                        allObjects.add(enemy);
+                    }
+                    if (b.hasFired()) {
+                        GameObject ammo = MunitionFactory.getNewMunition(b.getGrid(), b.getPosition(), b);
 
-                    allObjects.add(enemy);
-                }
-                if (b.hasFired()) {
-                    GameObject ammo = MunitionFactory.getNewMunition(b.getGrid(), b.getPosition(), b);
+                        allObjects.add(ammo);
 
-                    allObjects.add(ammo);
-
-                    b.reset_fired();
-                }
-                for (int i = 0; i < allObjects.size(); i++) {
-                    boolean isToRemove = false;
+                        b.reset_fired();
+                    }
+                    for (int i = 0; i < allObjects.size(); i++) {
+                        boolean isToRemove = false;
 
 
-                    if (allObjects.get(i) instanceof Munition) {
-                        Munition object = (Munition) allObjects.get(i);
-                        if (!object.isHide()) {
-                            object.move();
-                        } else {
+                        if (allObjects.get(i) instanceof Munition) {
+                            Munition object = (Munition) allObjects.get(i);
+                            if (!object.isHide()) {
+                                object.move();
+                            } else {
 
-                            isToRemove = true;
+                                isToRemove = true;
+                            }
+
                         }
 
-                    }
-
-                    if (allObjects.get(i) instanceof PlayerPlane) {
-                        PlayerPlane object = (PlayerPlane) allObjects.get(i);
-                        if (object.isHide()) {
-                            b.setDead();
-                            object.explosion(object.getPosition().getCol() * 10, object.getPosition().getRow() * 10 );
+                        if (allObjects.get(i) instanceof PlayerPlane) {
+                            PlayerPlane object = (PlayerPlane) allObjects.get(i);
+                            if (object.isHide()) {
+                                b.setDead();
+                                object.explosion(object.getPosition().getCol() * 10, object.getPosition().getRow() * 10);
+                            }
                         }
-                    }
 
-                    if (allObjects.get(i) instanceof EnemyPlane) {
-                        EnemyPlane object = (EnemyPlane) allObjects.get(i);
+                        if (allObjects.get(i) instanceof EnemyPlane) {
+                            EnemyPlane object = (EnemyPlane) allObjects.get(i);
 
                             if (!object.isHide()) {
                                 object.move();
@@ -93,14 +98,15 @@ public class Main {
 
                                     System.out.println(object.isDead());
                                 }
-                            }else {
-                                    if (object.getPosition().getRow() >= 0) {
-                                        object.explosion(object.getPosition().getCol() * 10, object.getPosition().getRow() * 10 );
-                                     counter += 1;
+                            } else {
+                                if (object.getPosition().getRow() >= 0) {
+                                    object.explosion(object.getPosition().getCol() * 10, object.getPosition().getRow() * 10);
+                                    counter += 1;
 
-                                    }
-                                    System.out.println("is dead");
-                                    isToRemove = true;
+                                }
+                                System.out.println("is dead");
+                                isToRemove = true;
+
 
                             }
 
@@ -110,41 +116,47 @@ public class Main {
                                 object.reset_fired();
                             }
 
+                        }
+
+                      if (isToRemove) {
+                            allObjects.remove(allObjects.get(i));
+
+                        }
+
                     }
 
-                    if (isToRemove) {
-                        allObjects.remove(allObjects.get(i));
-
-                    }
-
-                }
-
-                isThereAColission.detected(allObjects);
+                    isThereAColission.detected(allObjects);
 
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
-            if (b.isMoving()) {
-                b.move();
-                arena.move();
-
-                try {
-                    Thread.sleep(30);
                 } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+
+
+                if (b.isMoving()) {
+                    b.move();
+                    arena.move();
+
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                    }
+
+                }
+
+                score.delete();
 
             }
 
-
-        }
+        System.out.println(levelDiff);
         System.out.println("fuck you --> score: " + counter);
         Text score = new Text(250, 500, "Score: " + counter);
         score.grow(150,150);
         score.setColor(Color.DARK_GRAY);
         score.draw();
+
+
+
 
     }
 
